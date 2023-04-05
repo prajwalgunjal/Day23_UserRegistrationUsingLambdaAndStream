117977e2e6d10ffbb20800c87f1559548c1082f2
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-05 14:41:30 +0530

    UC9 Added

--- test/java/com/bridgelabz/UserRegistrationUsingLambdaTest.java
+++ test/java/com/bridgelabz/UserRegistrationUsingLambdaTest.java
@@ -37,6 +37,6 @@ public class UserRegistrationUsingLambdaTest {
     void GivenPasswordShouldWrittenTrue(){
         boolean password = userRegistration.ValidatePassword("Prajwall23");
         Assertions.assertTrue(password);
-        System.out.println("Password Checked (Rule 1 And 2 and 3 and 4 Checked)");
+        System.out.println("Password Checked (Rule 1 And 2 and 3 and 4 Checked) all sample passed");
     }
 }
